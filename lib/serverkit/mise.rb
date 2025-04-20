26263107cfadd249c2add22a76f30db232958875
toshimaru <me@toshimaru.net>
2025-04-20 16:06:10 +0900

    Init gem

---
+++ lib/serverkit/mise.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "mise/version"
+
+module Serverkit
+  module Mise
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/serverkit/mise/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Serverkit
+  module Mise
+    VERSION = "0.1.0"
+  end
+end
